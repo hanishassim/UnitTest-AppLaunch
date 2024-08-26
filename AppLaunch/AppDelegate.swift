da82fef0521f1dcbbbd76126451b8e205f25dda1
hanishassim <4571502+hanishassim@users.noreply.github.com>
2024-08-26 09:36:30 +0800

    Update comments

--- AppLaunch/AppDelegate.swift
+++ AppLaunch/AppDelegate.swift
@@ -14,6 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         print(">> Launching with real app delegate")
+        
+        /// Move anthing unrelated to app lifecycle out from app delegate, like placing into their own singletons
+        
         return true
     }
 
--- AppLaunchTests/TestingAppDelegate.swift
+++ AppLaunchTests/TestingAppDelegate.swift
@@ -11,11 +11,15 @@ import UIKit
 /// How to Switch Your iOS App and Scene Delegates for Improved Testing
 /// https://hacknicity.medium.com/how-to-switch-your-ios-app-and-scene-delegates-for-improved-testing-9746279378c3
 
-@objc(TestingAppDelegate) // To make it referable without needing a namespace
+@objc(TestingAppDelegate) /// To make it referable without needing a namespace
 class TestingAppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         print(">> Launching with testing app delegate")
+        
+        /// To consider in testingDelegate:
+        /// Set up core data with in-memory store to keep prod data from interfering with test data and make it run faster
+        /// Provide testing-specific key for analytics to avoid polluting collected data
+        
         return true
     }
 }
-
